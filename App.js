0ffab972729b1b10ffcd9a0c640350285da259cc
Nauman Husnani <naumanhusnani@gmail.com>
2020-01-21 19:13:11 -0800

    made into a stateful class-based component for future manipulation

--- App.js
+++ App.js
@@ -10,13 +10,22 @@ import React from "react"
 import TodoItem from "./TodoItem"
 import todosData from "./todosData"
 
-function App() {
-    const todoComponents = todosData.map(item => <TodoItem key={item.id} item={item} />)
-    return (
-        <div className="todo-list">
-            {todoComponents}
-        </div>
-    )
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            todos: todosData
+        }
+    }
+    render() {
+        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item}/>)
+
+        return (
+            <div className="todo-list">
+                {todoItems}
+            </div>
+        )
+    }
 }
 
 export default App
